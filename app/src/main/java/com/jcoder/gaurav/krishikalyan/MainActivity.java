245017b02956a4ec9d94184f5a6ef41430ce3949
Gaurav sahu <gaurav.sahu34@gmail.com>
2018-02-07 23:23:07 +0530

    Home page is created

---
+++ app/src/main/java/com/jcoder/gaurav/krishikalyan/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jcoder.gaurav.krishikalyan;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
